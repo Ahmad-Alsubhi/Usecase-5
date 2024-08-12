49fd4d0a0b8a5c126be769707873a627e1341796
Ahmad-Alsubhi <contact.alsubhi@gmail.com>
2024-08-12 04:44:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,13 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import zscore
+from ydata_profiling import ProfileReport
+import sweetviz as sv
+import plotly as pl
+import plotly.graph_objects as go
+import altair as alt
+
 st.write("hi")
